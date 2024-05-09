38d1f31942f7c250b06154027a78440a950a80a1
Ajith <ajith.a@digiaplus.com>
2024-05-09 18:07:23 +0530

    snackbar added

--- app/src/main/java/com/example/applib/MainActivity.kt
+++ app/src/main/java/com/example/applib/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.applib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.example.myappsamplelibrary.SimpleLibrary
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
         SimpleLibrary.simpleToast(applicationContext, "Hello this is simple toast... 👍 👍 👍")
+
+        button.setOnClickListener {
+            SimpleLibrary.simpleSnackBar(button)
+        }
     }
 }
--- myappsamplelibrary/src/main/java/com/example/myappsamplelibrary/SimpleLibrary.kt
+++ myappsamplelibrary/src/main/java/com/example/myappsamplelibrary/SimpleLibrary.kt
@@ -1,11 +1,24 @@
 package com.example.myappsamplelibrary
 
 import android.content.Context
+import android.graphics.Color
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 
 object SimpleLibrary {
     fun simpleToast(context: Context?, msg: String?) {
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    fun simpleSnackBar(view: View) {
+        val snackBar = Snackbar.make(view, "Hello...", Snackbar.LENGTH_LONG).setAction("Welcome") { }
+        snackBar.setActionTextColor(Color.RED)
+        val snackBarView = snackBar.view
+        val textView = snackBarView.findViewById<View>(com.google.android.material.R.id.snackbar_text) as TextView
+        textView.setTextColor(Color.YELLOW)
+        snackBar.show()
     }
 }
